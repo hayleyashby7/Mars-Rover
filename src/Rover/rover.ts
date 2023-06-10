3ab93dcd360bd6ffd6e274b480e800c77110694e
Hayley Ashby <hayleyashby7@gmail.com>
2023-06-10 11:10:58 +0100

    Test - create Rover - gives a default ID if none passed in - passing

--- src/Rover/rover.ts
+++ src/Rover/rover.ts
@@ -1,6 +1,7 @@
 import { Rover } from './rover.d';
 
-export const createRover = (id: number): Rover => {
+export const createRover = (id: number = 0): Rover => {
 	const newRover: Rover = { ID: id };
+
 	return newRover;
 };
