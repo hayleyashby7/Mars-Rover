cc526625a62b67c89bee2010d054ccb321c0869e
Hayley Ashby <hayleyashby7@gmail.com>
2023-06-10 11:06:56 +0100

    Test - create Rover - created Rover ID matches ID passed in - passing

--- src/Rover/rover.test.ts
+++ src/Rover/rover.test.ts
@@ -9,4 +9,16 @@ describe('Rover', () => {
 		// Assert
 		expect(result).toBeDefined();
 	});
+
+	it('has an ID, that matches the ID provided when created', () => {
+		// Arrange
+		const expectedID: number = 1;
+
+		// Act
+		const result: Rover = createRover(expectedID);
+
+		// Assert
+		expect(result.ID).toBeDefined();
+		expect(result.ID).toEqual(expectedID);
+	});
 });
