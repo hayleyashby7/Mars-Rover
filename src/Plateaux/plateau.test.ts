18f89869825d6ca609cd94e4860178bd8442ac7b
Hayley Ashby <hayleyashby7@gmail.com>
2023-06-12 19:12:29 +0100

    Test - Check if location is on plateau - failing

--- src/Plateaux/plateau.test.ts
+++ src/Plateaux/plateau.test.ts
@@ -1,5 +1,6 @@
 import { Plateau } from './plateau.d';
 import { createPlateau } from './plateau';
+import { Position } from '../utils/position';
 describe('Plataeu', () => {
 	it('can be created', () => {
 		//Act
@@ -9,18 +10,30 @@ describe('Plataeu', () => {
 		expect(result).toBeDefined();
 	});
 
-    it('can be created with a defined height and width', () => { 
-        //Arrange
-        const expectedHeight: number = 5;
-        const expectedWidth: number = 5;
+	it('can be created with a defined height and width', () => {
+		//Arrange
+		const expectedHeight: number = 5;
+		const expectedWidth: number = 5;
 
-        //Act
-        const result: Plateau = createPlateau(expectedHeight, expectedWidth);
+		//Act
+		const result: Plateau = createPlateau(expectedHeight, expectedWidth);
 
-        //Assert
-        expect(result.height).toBeDefined();
-        expect(result.height).toEqual(expectedHeight);
-        expect(result.width).toBeDefined();
-        expect(result.width).toEqual(expectedWidth);
-    });
+		//Assert
+		expect(result.height).toBeDefined();
+		expect(result.height).toEqual(expectedHeight);
+		expect(result.width).toBeDefined();
+		expect(result.width).toEqual(expectedWidth);
+	});
+
+	it('can confirm if a grid location is valid', () => {
+		//Arrange
+		const plateau: Plateau = createPlateau(5, 5);
+		const position: Position = { x: 0, y: 0 };
+
+		//Act
+		const validResult: boolean = plateau.isValidLocation(position);
+
+		//Assert
+		expect(validResult).toBe(true);
+	});
 });
