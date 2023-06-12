92d737f78ff9c07c48c664d1e772ba7032f9c66f
Hayley Ashby <hayleyashby7@gmail.com>
2023-06-12 19:05:16 +0100

    Test - Create Plateau with Dimensions - passing

--- src/Plateaux/plateau.ts
+++ src/Plateaux/plateau.ts
@@ -1,6 +1,6 @@
 import { Plateau } from './plateau.d';
 
-export const createPlateau = (): Plateau => {
-	const plateau: Plateau = { height: 0, width: 0 };
+export const createPlateau = (height: number = 0, width: number = 0): Plateau => {
+	const plateau: Plateau = { height: height, width: width };
 	return plateau;
 };
