4853638ff65a39b7555b0b73eac32ef86f3853be
Hayley Ashby <hayleyashby7@gmail.com>
2023-06-12 19:03:07 +0100

    Test - Create Plateau with Dimensions - failing

--- src/Plateaux/plateau.test.ts
+++ src/Plateaux/plateau.test.ts
@@ -8,4 +8,19 @@ describe('Plataeu', () => {
 		//Assert
 		expect(result).toBeDefined();
 	});
+
+    it('can be created with a defined height and width', () => { 
+        //Arrange
+        const expectedHeight: number = 5;
+        const expectedWidth: number = 5;
+
+        //Act
+        const result: Plateau = createPlateau(expectedHeight, expectedWidth);
+
+        //Assert
+        expect(result.height).toBeDefined();
+        expect(result.height).toEqual(expectedHeight);
+        expect(result.width).toBeDefined();
+        expect(result.width).toEqual(expectedWidth);
+    });
 });
