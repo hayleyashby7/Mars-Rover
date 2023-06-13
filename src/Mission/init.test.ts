60560ec3ee5f36bf73bce5b3c8d1a05c69969d0e
Hayley Ashby <hayleyashby7@gmail.com>
2023-06-13 20:03:42 +0100

    Test - Initialise Mission takes input to set number of Rovers - failing

--- src/Mission/init.test.ts
+++ src/Mission/init.test.ts
@@ -27,18 +27,6 @@ describe('Initialise Mission', () => {
 		expect(mission.vehicles).toBeDefined();
 	});
 
-	it('should initialise a mission with a single Rover in the vehicles array', () => {
-		// Arrange
-		const expectedVehicle: Rover = createRover();
-
-		// Act
-		const mission: Mission = initialiseMission();
-
-		// Assert
-		expect(mission.vehicles.length).toBe(1);
-		expect(mission.vehicles[0]).toMatchObject(expectedVehicle);
-	});
-
 	it('should use input to determine the size of the plateau', () => {
 		// Arrange
 		const expectedPlateauHeight: number = 5;
@@ -51,5 +39,17 @@ describe('Initialise Mission', () => {
 		// Assert
 		expect(mission.plateau.height).toBe(expectedPlateauHeight);
 		expect(mission.plateau.width).toBe(expectedPlateauWidth);
+	});
+
+	it('should use input to determine the number of rovers', () => {
+		// Arrange
+		const expectedNumberOfRovers: number = 2;
+		const testInput: string = `5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM`;
+
+		// Act
+		const mission: Mission = initialiseMission(testInput);
+
+		// Assert
+		expect(mission.vehicles.length).toBe(expectedNumberOfRovers);
 	});
 });
