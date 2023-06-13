342bf138be5652044f9b87d98795456e0dc929aa
Hayley Ashby <hayleyashby7@gmail.com>
2023-06-13 19:11:50 +0100

    Test - Initialise Mission starts with 1 rover in Vehicles array - passing

--- src/Mission/init.ts
+++ src/Mission/init.ts
@@ -1,7 +1,8 @@
 import { Mission } from '../types.d';
+import { createRover } from '../Rover/rover';
 import { createPlateau } from '../Plateau/plateau';
 
 export const initialiseMission = (): Mission => {
-	const mission: Mission = { plateau: createPlateau(), vehicles: [] };
+	const mission: Mission = { plateau: createPlateau(), vehicles: [createRover()] };
 	return mission;
 };
