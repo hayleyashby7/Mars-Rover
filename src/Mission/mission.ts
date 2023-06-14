deabd0d5a16e7b0e1b3bcff8e3bdc1929b54de1d
Hayley Ashby <hayleyashby7@gmail.com>
2023-06-14 20:36:29 +0100

    Revert "Revert "Test - Rover will not move off the plateau - Passing""
    This reverts commit 1cc5f0f097a72222403f9a1e92e2048d9043fe30.

--- src/Mission/mission.ts
+++ src/Mission/mission.ts
@@ -1,5 +1,6 @@
+import { occupyLocation, vacateLocation } from '../Plateau/plateau';
 import { Rover } from '../Rover/rover';
-import { Input } from '../types';
+import { Input, Plateau } from '../types';
 import { parseInput } from '../utils/input';
 import { initialiseMission } from './init';
 
@@ -8,18 +9,37 @@ export const runMission = (input: string): string => {
 	const mission = initialiseMission(parsedInput);
 	let output: string = '';
 
+	if (!mission) return output;
+	if (!mission.Plateau) return output;
+	if (!mission.Vehicles) return output;
+
 	for (let i = 0; i < mission.Vehicles.length; i++) {
-		performRoverMoves(mission.Vehicles[i], parsedInput.VehicleInputs[i].moves);
+		performRoverMoves(mission.Plateau, mission.Vehicles[i], parsedInput.VehicleInputs[i].moves);
 		output += `${mission.Vehicles[i].Position.x} ${mission.Vehicles[i].Position.y} ${mission.Vehicles[i].Direction}\n`;
 	}
 
 	return output;
 };
 
-export const performRoverMoves = (rover: Rover, moves: string): void => {
+export const performRoverMoves = (plateau: Plateau, rover: Rover, moves: string): void => {
 	const movesArray = moves.split('');
 
 	movesArray.forEach((move: string) => {
+		if (move === 'M' && !isMoveValid(plateau, rover)) {
+			throw new Error(`Move ${move} would send rover off plateau!`);
+		}
+		const oldPosition = rover.Position;
 		rover.move(move);
+		vacateLocation(plateau, oldPosition);
+		occupyLocation(plateau, rover.Position);
 	});
+};
+
+const isMoveValid = (plateau: Plateau, rover: Rover): boolean => {
+	const testRover = new Rover(rover.ID, rover.Position, rover.Direction);
+	testRover.move('M');
+
+	if (!plateau.isValidLocation(plateau, testRover.Position)) return false;
+	if (!plateau.isLocationEmpty(plateau, testRover.Position)) return false;
+	return true;
 };
