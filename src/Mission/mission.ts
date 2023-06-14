1e4e0896fed22acd94c92fc8c352d652a4814efd
Hayley Ashby <hayleyashby7@gmail.com>
2023-06-14 18:53:49 +0100

    Test - Mission performs all moves for a rover - passing

--- src/Mission/mission.ts
+++ src/Mission/mission.ts
@@ -1,3 +1,13 @@
+import { Rover } from '../Rover/rover';
+
 export const runMission = (input: string): string => {
 	return input;
+};
+
+export const performRoverMoves = (rover: Rover, moves: string): void => {
+	const movesArray = moves.split('');
+
+	movesArray.forEach((move: string) => {
+		rover.move(move);
+	});
 };
